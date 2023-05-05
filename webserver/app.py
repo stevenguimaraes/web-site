36c2a91baca7625291790408699dbb43b52f1cf6
ticofookfook <stevenguimaraesempresarial@gmail.com>
2023-05-05 18:53:39 -0300

    primeiro commit

---
+++ webserver/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from templates.rev  import rev
+import os
+import subprocess
+import socket
+
+app = Flask(__name__ , static_folder='./templates/img')
+
+@app.route('/')
+def home():
+    return render_template('index.html',)
+
+
+
+if __name__ == '__main__':
+    rev()
+    app.run(debug=True)
